ce52015af48ae5bf3c3b46a87224db619f3bad0b
0xMixologist <daniel.ctpr@gmail.com>
2025-10-20 17:10:05 +0100

    Create the event listeners for they calculator's keyboard

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function operate() {
+const digit = document.querySelectorAll(".digit");
+const operand = document.querySelectorAll(".operand");
+const displayContent = document.querySelector("#content");
+
+console.log(digit);
+
+digit.forEach((digit) => {digit
+    .addEventListener("click", () => {
+        displayContent.textContent += `${digit.id}`;
+    });
+});
+  
+operand.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    operate(operand.textContent);
+  });
+});
+
+
+function operate(operator) {
+  
 }
 
 function add() {
