bd08b990808807cfd210fd9c44a6657899a07a1d
0xMixologist <daniel.ctpr@gmail.com>
2025-10-20 19:46:33 +0100

    Build the addition and subtraction functions

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 const digit = document.querySelectorAll(".digit");
 const operand = document.querySelectorAll(".operand");
 const displayContent = document.querySelector("#content");
+let calculation = [];
+let initialValue = null;
+let operator = null;
+let operation = false;
+let memory = null;
 
 console.log(digit);
 
 digit.forEach((digit) => {digit
     .addEventListener("click", () => {
-        displayContent.textContent += `${digit.id}`;
+        if (operation) {
+          displayContent.textContent = "";
+          displayContent.textContent += `${digit.id}`;
+          operation = false;
+        } else {
+          displayContent.textContent += `${digit.id}`;
+        }
+        if (calculation.length > 0) {
+          initialValue = calculation[0];
+        };
     });
 });
   
 operand.forEach((operand) => {
   operand.addEventListener("click", () => {
-    operate(operand.textContent);
+    calculation.push(parseInt(displayContent.textContent));
+    operation = true;
+    operator = operand.textContent;
+    if (memory !== null && operator !== memory) {
+      initialValue = null;
+      console.log(operate(memory, 1));
+    }
+      
+    if (initialValue !== null) {
+      console.log(operate(operator, 0));
+    }
+    return;
   });
 });
 
 
-function operate(operator) {
+function operate(operator, last) {
   switch(operator) {
     case "+":
-      add();
+      console.log(add(calculation, last));
       break;
     case "-":
-      subtract();
+      console.log(subtract(calculation, last));
       break;
     case "/":
       divide();
@@ -37,12 +62,30 @@ function operate(operator) {
 }
   
 
+
+
+function add(array, last) {
+  //result = calculation.reduce((accumulator, current) => accumulator + current, 0);
+  result = calculation[0] + calculation[1];
+  calculation = [result];
+  if (last) {
+    memory = null;
+    return result;
+  };
+  memory = "+";
+  return result; 
+
 }
 
-function add() {
-}
-
-function subtract() {
+function subtract(array, last) {
+  result = calculation[0] - calculation[1];
+  calculation = [result];
+  if (last) {
+    memory = null;
+    return result;
+  };
+  memory = "-";
+  return result;
 }
 
 function divide() {
