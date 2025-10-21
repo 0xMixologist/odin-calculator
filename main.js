a8b446ad0b7dac8e23aa76d871a386aba5fd3728
0xMixologist <daniel.ctpr@gmail.com>
2025-10-21 20:02:31 +0100

    Add float point rounding

--- main.js
+++ main.js
@@ -4,27 +4,25 @@ const clearButton = document.querySelectorAll(".clear-button");
 const displayContent = document.querySelector("#content");
 
 let calculation = [];
-let initialValue = null;
 let operator = null;
-let operation = false;
+let operation = true;
 let memory = null;
 
 console.log(digit);
 console.log(typeof(displayContent.textContent));
 console.log(displayContent.textContent);
 
+//This adds the numbers to the display 
+
 digit.forEach((digit) => {digit
     .addEventListener("click", () => {
-        if (operation) {
+        if (!operation) {
           displayContent.textContent = "";
           displayContent.textContent += `${digit.id}`;
-          operation = false;
+          operation = true;
         } else {
           displayContent.textContent += `${digit.id}`;
         }
-        if (calculation.length > 0) {
-          initialValue = calculation[0];
-        };
     });
 });
   
@@ -32,37 +30,42 @@ operand.forEach((operand) => {
   operand.addEventListener("click", () => {
     console.log("clicked an operator")
     if (displayContent.textContent !== "") {
-      calculation.push(parseInt(displayContent.textContent));
-      console.log("before = got pressed" + calculation);
-      operation = true;
-      operator = operand.textContent;
-      if (operator === "=") {
+      operator = operand.textContent;   //saves the operator 
+      if (operation) {
+        calculation.push(parseFloat(displayContent.textContent));
+      } else {
+        memory = operator;
+      }
+
+
+      if (operator === "=") {           //if the user presses equal, it will finish the calculation and return to the initial state
         console.log(operate(memory, 1));
         memory = null;
         operator = null;
         console.log(calculation);
-        calculation = [];
-        initialValue = null;
+        calculation = [parseFloat(displayContent.textContent)];
+        operation = false;
         return;
       }
       
-      if (memory === null) {
+      if (memory === null) {       //this happens in the initial state
         memory = operator;
       }
-
-      if (memory !== null && operator !== memory) {
-        initialValue = null;
+      console.log(operator);
+      console.log(memory);
+      if (memory !== null && operator !== memory && calculation.length > 1 && operation) {
         console.log(operate(memory, 1));
-        memory = operator;//calculates the pending operation when swithicng operator
+        memory = operator;   //calculates the pending operation when swithicng operator
       }
         
-      if (initialValue !== null) {
+      if (calculation.length > 1 && operation) {
         console.log(operate(operator, 0));
       }
+      operation = false;
       return;
     }
     else {
-      console.log("please dont");
+      return;
     }
     return;
   });
@@ -73,7 +76,6 @@ clearButton.forEach((button) => {
     switch(button.id) {
       case "ac":
         memory = null;
-        initialValue = null;
         operator = null;
         operation = false;
         calculation = [];
@@ -91,16 +93,16 @@ clearButton.forEach((button) => {
 function operate(operator, last) {
   switch(operator) {
     case "+":
-      displayContent.textContent = `${add(calculation, last)}`;
+      displayContent.textContent = Math.round(add(calculation, last) * 1000000) / 1000000;
       break;
     case "-":
-      displayContent.textContent = `${subtract(calculation, last)}`;
+      displayContent.textContent = Math.round(subtract(calculation, last) * 1000000) / 1000000;
       break;
     case "/":
-      displayContent.textContent = `${divide(calculation, last)}`;
+      displayContent.textContent = Math.round(divide(calculation, last) * 1000000) / 1000000;
       break;
     case "*":
-      displayContent.textContent = `${multiply(calculation, last)}`;
+      displayContent.textContent = Math.round(multiply(calculation, last) * 1000000) / 1000000;
       break;
     default:
       console.log("Invalid Operator");
@@ -118,7 +120,6 @@ function add(array, last) {
     memory = null;
     return result;
   };
-  memory = "+";
   return result; 
 
 }
@@ -127,7 +128,6 @@ function subtract(array, last) {
   result = calculation[0] - calculation[1];
   calculation = [result];
   if (last) {
-    memory = null;
     return result;
   };
   memory = "-";
@@ -138,7 +138,6 @@ function divide(array, last) {
   result = calculation[0] / calculation[1];
   calculation = [result];
   if (last) {
-    memory = null;
     return result;
   };
   memory = "/";
@@ -149,7 +148,6 @@ function multiply(array, last) {
   result = calculation[0] * calculation[1];
   calculation = [result];
   if (last) {
-    memory = null;
     return result;
   };
   memory = "*";
